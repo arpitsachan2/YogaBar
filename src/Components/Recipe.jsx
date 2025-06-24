c5c02a3a3ca48efde7026584e399471fe3230c0f
Arpit <arpitsachan2323@gmail.com>
2025-06-24 13:01:36 +0530

    ar

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -7,7 +7,7 @@ import img3 from '../assets/files/Granola_Fruit_Tart_Recipe-_Compressify.io.webp
 import img4 from '../assets/files/Butter_Apple_Sandwich_Recipe_1_-_Compressify.io.webp'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-const recipe = () => {
+const Recipe = () => {
   return (
     <div id='qq' className=''>
 
@@ -24,89 +24,90 @@ const recipe = () => {
             slidesPerView={2.6}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
+
           >
-            
 
-              <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
-                <img className='' src={img1} alt="" />
-                <div className='ml-[2vw] mt-[1vw]'>
-                  <h2 className='text-[1.5vw]  font-bold'>Stuffed Baked Apple</h2> <br />
-                  <h3>⭐️⭐️⭐️⭐️⭐️</h3>
-                  <span>⏰ 5</span>
+
+            <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
+              <img className='' src={img1} />
+              <div className='ml-[2vw] mt-[1vw]'>
+                <h2 className='text-[1.5vw]  font-bold'>Stuffed Baked Apple</h2> <br />
+                <h3>⭐️⭐️⭐️⭐️⭐️</h3>
+                <span>⏰ 5</span>
+              </div>
+              <div className='h-[5vw]  flex justify-center  '>
+
+                <div className='bg-black  text-white flex justify-center items-center 
+            rounded-3xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
+                  <span  > View Recipe  </span>
                 </div>
-                <div className='h-[5vw]  flex justify-center  '>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
+              <img className='' src={img2} />
+              <div className='ml-[2vw] mt-[1vw]'>
+                <h2 className='text-[1.5vw]  font-bold'>Peanut Butter Banana Popsicle</h2> <br />
+                <h3>⭐️⭐️⭐️⭐️⭐️</h3>
+                <span>⏰ 5</span>
+              </div>
+              <div className='h-[5vw]  flex justify-center  '>
 
-                  <div className='bg-black  text-white flex justify-center items-center 
+                <div className='bg-black  text-white flex justify-center items-center 
+            rounded-3xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
+                  <span  > View Recipe  </span>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
+              <img className='' src={img3} />
+              <div className='ml-[2vw] mt-[1vw]'>
+                <h2 className='text-[1.5vw]  font-bold'>Granola Fruit Tarts</h2> <br />
+                <h3>⭐️⭐️⭐️⭐️⭐️</h3>
+                <span>⏰ 5</span>
+              </div>
+              <div className='h-[5vw]  flex justify-center  '>
+
+                <div className='bg-black  text-white flex justify-center items-center 
             rounded-4xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
-                    <span  > View Recipe  </span>
-                  </div>
+                  <span  > View Recipe  </span>
                 </div>
-              </SwiperSlide>
-              <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
-                <img className='' src={img2} alt="" />
-                <div className='ml-[2vw] mt-[1vw]'>
-                  <h2 className='text-[1.5vw]  font-bold'>Peanut Butter Banana Popsicle</h2> <br />
-                  <h3>⭐️⭐️⭐️⭐️⭐️</h3>
-                  <span>⏰ 5</span>
-                </div>
-                <div className='h-[5vw]  flex justify-center  '>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
+              <img className='' src={img4} />
+              <div className='ml-[2vw] mt-[1vw]'>
+                <h2 className='text-[1.5vw]  font-bold'>Butter Apple Sandwich</h2> <br />
+                <h3>⭐️⭐️⭐️⭐️⭐️</h3>
+                <span>⏰ 5</span>
+              </div>
+              <div className='h-[5vw]  flex justify-center  '>
 
-                  <div className='bg-black  text-white flex justify-center items-center 
-            rounded-4xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
-                    <span  > View Recipe  </span>
-                  </div>
+                <div className='bg-black  text-white flex justify-center items-center 
+            rounded-3xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
+                  <span  > View Recipe  </span>
                 </div>
-              </SwiperSlide>
-              <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
-                <img className='' src={img3} alt="" />
-                <div className='ml-[2vw] mt-[1vw]'>
-                  <h2 className='text-[1.5vw]  font-bold'>Granola Fruit Tarts</h2> <br />
-                  <h3>⭐️⭐️⭐️⭐️⭐️</h3>
-                  <span>⏰ 5</span>
-                </div>
-                <div className='h-[5vw]  flex justify-center  '>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
+              <img className='' src={img5} />
+              <div className='ml-[2vw] mt-[1vw]'>
+                <h2 className='text-[1.5vw]  font-bold'>Nut Butter Chickpeas</h2> <br />
+                <h3>⭐️⭐️⭐️⭐️⭐️</h3>
+                <span>⏰ 5</span>
+              </div>
+              <div className='h-[5vw]  flex justify-center  '>
 
-                  <div className='bg-black  text-white flex justify-center items-center 
-            rounded-4xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
-                    <span  > View Recipe  </span>
-                  </div>
+                <div className='bg-black  text-white flex justify-center items-center 
+            rounded-3xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
+                  <span  > View Recipe  </span>
                 </div>
-              </SwiperSlide>
-              <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
-                <img className='' src={img4} alt="" />
-                <div className='ml-[2vw] mt-[1vw]'>
-                  <h2 className='text-[1.5vw]  font-bold'>Butter Apple Sandwich</h2> <br />
-                  <h3>⭐️⭐️⭐️⭐️⭐️</h3>
-                  <span>⏰ 5</span>
-                </div>
-                <div className='h-[5vw]  flex justify-center  '>
-
-                  <div className='bg-black  text-white flex justify-center items-center 
-            rounded-4xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
-                    <span  > View Recipe  </span>
-                  </div>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className='bg-[#F8F2E3] h-[80vw] rounded-b-xl   ' >
-                <img className='' src={img5} alt="" />
-                <div className='ml-[2vw] mt-[1vw]'>
-                  <h2 className='text-[1.5vw]  font-bold'>Nut Butter Chickpeas</h2> <br />
-                  <h3>⭐️⭐️⭐️⭐️⭐️</h3>
-                  <span>⏰ 5</span>
-                </div>
-                <div className='h-[5vw]  flex justify-center  '>
-
-                  <div className='bg-black  text-white flex justify-center items-center 
-            rounded-4xl hover:border-black border-[#F8F2E2] border-2 cursor-pointer text-[1.4vw] font-bold  h-[3.5vw] w-[28vw]    '>
-                    <span  > View Recipe  </span>
-                  </div>
-                </div>
-              </SwiperSlide>
+              </div>
+            </SwiperSlide>
 
 
 
 
-            
+
           </Swiper>
         </div>
       </div>
@@ -115,4 +116,4 @@ const recipe = () => {
   )
 }
 
-export default recipe
+export default Recipe
